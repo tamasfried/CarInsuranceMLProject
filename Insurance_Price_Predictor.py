1ae5fcbb2af05f74b1c72221781a91aa6527cda1
tamasfried <tomwfmorton@gmail.com>
2025-06-15 20:31:33 +0100

    Final product + PDF

--- Insurance_Price_Predictor.py
+++ Insurance_Price_Predictor.py
@@ -1,11 +1,14 @@
-# Imports
+# Install required libraries if not already installed from requirements.txt
+# pip install -r requirements.txt (in this directory)
+# or manually: pip install pandas, numpy, scikit-learn, matplotlib
+
+# Import necessary libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def prompt_user_for_features():
     # Function to collect driver and vehicle information from user input
